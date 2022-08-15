77a84ec1e3f663cd46ea1bac970a9fa057615003
Burak Can Akıncı <burakakinci.bca@gmail.com>
2022-08-15 12:31:05 +0300

    Initial commit

---
+++ Architecture-Portfolio/client/pages/Gallery.vue
@@ -0,0 +1,722 @@
+<template>
+     <div class="portfolio background-color-gallery" style="padding: 2.5em;">
+  <h2 class="title">Latest Projects</h2>
+  <a href="#" class="card space">
+    <div class="content">
+      <span class="title">Web Design</span>
+      <span class="category">Mobile / Design / Rebranding</span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/1.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card space">
+    <div class="content">
+      <span class="title">UX Design</span>
+      <span class="category">UX / UI / Research </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/Picture4.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card space">
+    <div class="content">
+      <span class="title">Concepts</span>
+      <span class="category">Design / Creative</span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/2.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card space">
+    <div class="content">
+      <span class="title">Creative & Cool</span>
+      <span class="category">Design / Food</span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/3.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card space">
+    <div class="content">
+      <span class="title">AR-VR</span>
+      <span class="category">VR / AR / Creative</span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/6.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card space">
+    <div class="content">
+      <span class="title">Logo</span>
+      <span class="category">Design / Logo / Clothing</span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/5.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card space">
+    <div class="content">
+      <span class="title">Art</span>
+      <span class="category">Drawing / Painting / Abstract</span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/7.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card space">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/8.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card space">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/9.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card space">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/10.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card space">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/11.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card space">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/12.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/13.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/14.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/15.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/16.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/17.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/18.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/19.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/20.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/21.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/22.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/23.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/24.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/25.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/26.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/27.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/28.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/29.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/30.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/31.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/32.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/33.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/34.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/35.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/36.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/40.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/38.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/39.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/41.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/42.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/43.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/44.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/45.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/46.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/47.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/48.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/49.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/50.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/51.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/52.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/53.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/54.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/55.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/56.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/57.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/58.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/59.jpg" alt="" />
+    </div>
+  </a>
+
+  <a href="#" class="card">
+    <div class="content">
+      <span class="title">Native Apps</span>
+      <span class="category">Coding / Mobile </span>
+    </div>
+    <div class="image">
+      <img src="~/assets/image/Gallery/60.jpg" alt="" />
+    </div>
+  </a>
+
+
+
+</div>
+</template>
+<script>
+  export default {
+    data: () => ({
+    
+    }),
+    mounted() {
+      const recaptchaScript = document.createElement('script')
+      recaptchaScript.setAttribute('src', 'https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=61129bb2ecedf803564c68ec')
+      document.head.appendChild(recaptchaScript)
+
+      const deneme = document.createElement('script')
+      deneme.setAttribute('src', 'https://assets.website-files.com/61129bb2ecedf803564c68ec/js/webflow.19742290c.js')
+      document.head.appendChild(deneme)
+    },
+    methods: {
+      
+    }
+  }
+</script>
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+}
+.portfolio {
+  display: flex;
+  flex-wrap: wrap;
+  min-width: 320px;
+}
+
+.portfolio h2 {
+  flex-basis: 100%;
+  text-align: center;
+  margin: 50px auto 30px;
+  text-transform: uppercase;
+  font-size: 20px;
+  letter-spacing: 2px;
+  color: #111;
+}
+.card {
+  width: 25%;
+  overflow: hidden;
+  position: relative;
+}
+.card .content {
+  z-index: 2;
+  width: 100%;
+  position: absolute;
+  bottom: -100px;
+  transition: all 0.7s ease;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+  box-sizing: border-box;
+  min-height: 100px;
+  background: #111;
+}
+.card .image {
+  z-index: 1;
+  height: 100%;
+}
+.card img {
+  height: 100%;
+  width: 100%;
+  transition: all 0.5s ease;
+  transform: scale(1.2);
+}
+
+.card:hover .content {
+  bottom: 0px;
+  color: #fff;
+}
+
+.card:hover .image img {
+  transform: scale(1);
+}
+
+.card .content span:first-child {
+  text-transform: uppercase;
+  margin-bottom: 10px;
+  font-weight: 700;
+  letter-spacing: 1px;
+  text-align: center;
+  color: #fff;
+  font-size: 16px;
+}
+
+.card .content span:last-child {
+  font-size: 14px;
+  color: #18cfab;
+  text-align: center;
+  font-weight: 700;
+}
+
+
+@media screen and (max-width: 768px) {
+  .card {
+  width: 50%;
+  }
+  .card .content {
+    bottom: 0;
+  }
+}
+
+@media screen and (max-width: 480px) {
+  .card {
+  width: 100%;
+  }
+}
+
+.card{
+    border-radius: 0px!important;
+}
+
+.background-color-gallery {
+  background: linear-gradient(180deg, #6B705C 0%,  #CB997E 20%,
+                                      #A09881 40%, #FFE8D6 60%,
+                                      #807362 80%, #A4594C 99%);
+}
+</style>
